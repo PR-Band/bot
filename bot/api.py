37b43338842051786029ce2a42565eb7eac3570a
Andrey Rukin <rukin897@gmail.com>
2023-03-30 22:12:33 +0300

    feat: добавили регистрацию User

---
+++ bot/api.py
@@ -0,0 +1,24 @@
+import httpx
+
+
+# try, except  HTTPStatusError: Client error '409 CONFLICT'
+class UserClient:
+    def __init__(self, url: str):
+        self.url = f'{url}/api/v1/users/'
+
+    def registrate(self, username: str, tgid: int) -> None:
+
+        data = {'tgid': tgid, 'username': username}
+        # if
+        response = httpx.post(self.url, json=data)
+        response.raise_for_status()
+
+
+
+class ApiClient:
+
+    def __init__(self, url: str):
+
+        self.users = UserClient(url=url)
+
+api = ApiClient(url='http://127.0.0.1:8000')
