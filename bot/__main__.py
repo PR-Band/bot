b28b012eec72088e91ef071392090ccb0ca568b0
Stas Petrov <petrovst@mail.ru>
2023-04-08 16:19:58 +0300

    feat: хендлер addproduct готов

--- bot/__main__.py
+++ bot/__main__.py
@@ -6,7 +6,7 @@ from telegram.ext import CommandHandler, Updater
 from bot.api import api
 from bot.config import config
 from bot.errors import IncorrectAddCmdError
-from bot.products import add_product
+from bot.products import add_product, get_category_product
 
 # MessageHandler, Filters
 logger = logging.getLogger(__name__)
@@ -17,11 +17,39 @@ jenv = Environment(
 )
 
 
+def choose_category(update, context):
+    """
+    /choose Волейбол
+
+    продукт `{product}` добавлен в категорию `{category_title}`
+    """
+    cmd = update.message.text
+    category_name = ' '.join(cmd.split(' ')[1:])
+    categories = api.categories.get_categories_by_name(category_name)
+    category = categories[0]
+    product = context.user_data.get('product_name')
+    post = api.products.post_product(category['id'], product)
+    if not post:
+        update.message.reply_text('В категории уже есть такой продукт')
+        return
+    category_title = category['title']
+    update.message.reply_text(
+        f'продукт `{product}` добавлен в категорию `{category_title}`',
+        parse_mode='MarkdownV2',
+    )
+
+
 def add_product_in_handlers(update, context):
+    cmd = update.message.text
     try:
-        message = add_product(update.message.text)
+        category_name, product = get_category_product(cmd)
     except IncorrectAddCmdError as err:
-        update.message.reply_text(err.message, parse_mode='MarkdownV2')
+        update.message.reply_text(err.message)
+        return
+
+    context.user_data['product_name'] = product
+    message = add_product(category_name, product)
+
     update.message.reply_text(
         message,
         parse_mode='MarkdownV2',
@@ -44,10 +72,11 @@ def start(update, context):
 
 def main():
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=config.log_level)
     updater = Updater(token=config.api_key, use_context=True)
     dp = updater.dispatcher  # type: ignore
     dp.add_handler(CommandHandler('addproduct', add_product_in_handlers))
+    dp.add_handler(CommandHandler('choose', choose_category))
     dp.add_handler(CommandHandler('start', start))
     updater.start_polling()
 
--- bot/config.py
+++ bot/config.py
@@ -6,10 +6,11 @@ from dataclasses import dataclass
 class Config:
     api_key: str
     http_key: str
+    log_level: str
 
 
 def load():
-    return Config(api_key=os.environ['API_KEY'], http_key=os.environ['HTTP_KEY'])
+    return Config(api_key=os.environ['API_KEY'], http_key=os.environ['HTTP_KEY'], log_level=os.environ['LOG_LEVEL'],)
 
 
 config = load()
