af6fb69c96ba24432cb47eba6c5de7b60f6420e7
Stas Petrov <petrovst@mail.ru>
2023-04-10 02:35:38 +0300

    feat: ручка слотов в api, хендлер слотов в main

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,3 +1,4 @@
+import httpx
 import logging
 
 from jinja2 import Environment, PackageLoader, select_autoescape
@@ -70,6 +71,36 @@ def start(update, context):
     api.users.registrate(username=username, tgid=tgid)
 
 
+def parse_day_start_end(cmd) -> tuple[str, str, str]:
+    date_list = cmd.split(' ')
+    day = date_list[1]
+    start_time = date_list[2].split('-')[0]
+    end_time = date_list[2].split('-')[1]
+    return day, start_time, end_time
+
+
+def add_slot(update, context):
+    cmd = update.message.text
+    day, start_slot, end_slot = parse_day_start_end(cmd)
+    try:
+        api.scheduletemplate.post_schedule_templates(
+            day=day,
+            start_slot=start_slot,
+            end_slot=end_slot,
+            uid=1,
+            product_id=1,
+        )
+    except httpx.HTTPStatusError:
+        raise update.message.reply_text(
+            f'Слот `c {start_slot} до {end_slot}` уже занят',
+            parse_mode='MarkdownV2',
+        )
+    update.message.reply_text(
+        f'Слот `c {start_slot} до {end_slot}` добавлен',
+        parse_mode='MarkdownV2',
+    )
+
+
 def main():
 
     logging.basicConfig(level=config.log_level)
@@ -78,6 +109,7 @@ def main():
     dp.add_handler(CommandHandler('addproduct', add_product_in_handlers))
     dp.add_handler(CommandHandler('choose', choose_category))
     dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('addslot', add_slot))
     updater.start_polling()
 
 
